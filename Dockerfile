b83dfa61458f85134b6af9728d3a056f93553371
JP Posma <janpaul123@users.noreply.github.com>
2022-06-03 10:27:19 -0700

    Better logging; session id validation; socket timeout; fixed resizing

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:16-alpine
+
+RUN mkdir -p /app
+WORKDIR /app
+
+COPY package.json  .
+COPY package-lock.json .
+RUN npm install --production
+
+COPY app.js .
+COPY index.html .
+
+EXPOSE 80
+EXPOSE 1337
+
+CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import expressWs from "express-ws";
 import { Server } from 'net';
-import publicIp from "public-ip";
 import dotenv from "dotenv";
 import fs from "fs";
 import { v4 as uuidv4 } from 'uuid';
@@ -19,9 +18,12 @@ const remoteSocketsByShortSessionId = {};
 const webSocketsByShortSessionId = {};
 
 function generateLongSessionId() {
-  return [uuidv4(), uuidv4(), uuidv4(), uuidv4(), uuidv4()].join("").replace(/-/g, "");
+  return [uuidv4(), uuidv4(), uuidv4(), uuidv4(), uuidv4(), uuidv4(), uuidv4(), uuidv4(), uuidv4(), uuidv4()].join("").replace(/-/g, "");
 }
-// const longSessionIdLength = generateLongSessionId().length;
+const longSessionIdLength = generateLongSessionId().length;
+function isValidLongSessionId(longSessionId) {
+  return longSessionId.length === longSessionIdLength;
+}
 
 function shortSessionIdFromLongSessionId(longSessionId) {
   return hasha(longSessionId).substring(0, 16);
@@ -29,6 +31,9 @@ function shortSessionIdFromLongSessionId(longSessionId) {
 const shortSessionIdLength = shortSessionIdFromLongSessionId(generateLongSessionId()).length;
 
 function sendToWebsockets(shortSessionId, message) {
+  if (process.env.VERBOSE === "true") {
+    console.log(`Sending message to websockets: ${message}`);
+  }
   if (webSocketsByShortSessionId[shortSessionId]) {
     for (const webSocket of webSocketsByShortSessionId[shortSessionId]) {
       webSocket.send(message);
@@ -39,7 +44,9 @@ function sendToWebsockets(shortSessionId, message) {
 server.on('connection', function (socket) {
   let chunks = "";
   let shortSessionId = undefined;
-  console.log('New socket...');
+  if (process.env.VERBOSE === "true") {
+    console.log('New socket...');
+  }
   socket.on('data', function (chunk) {
     if (shortSessionId) {
       sendToWebsockets(shortSessionId, chunk);
@@ -48,14 +55,18 @@ server.on('connection', function (socket) {
       // Extract SSH_NOW_SHORT_SESSION_ID using shortSessionIdLength and store it in remoteSocketsByShortSessionId
       if (chunks.indexOf('SSH_NOW_SHORT_SESSION_ID=') > -1) {
         shortSessionId = chunks.split('SSH_NOW_SHORT_SESSION_ID=')[1].substring(0, shortSessionIdLength);
-        console.log(`New session: ${shortSessionId}`);
+        if (process.env.VERBOSE === "true") {
+          console.log(`New session: ${shortSessionId}`);
+        }
         remoteSocketsByShortSessionId[shortSessionId] = remoteSocketsByShortSessionId[shortSessionId] || [];
         remoteSocketsByShortSessionId[shortSessionId].push(socket);
         chunks = "";
         sendToWebsockets(shortSessionId, "");
       }
     }
-    console.log(`Data received from client: ${chunk.toString()}`);
+    if (process.env.VERBOSE === "true") {
+      console.log(`Data received from client: ${chunk.toString()}`);
+    }
   });
   socket.on('end', function () {
     if (shortSessionId && remoteSocketsByShortSessionId[shortSessionId]) {
@@ -71,6 +82,12 @@ server.on('connection', function (socket) {
       sendToWebsockets(shortSessionId, `SSH Now socket error: ${err.stack}`);
     }
   });
+  setTimeout(() => {
+    if (!shortSessionId) {
+      console.log(`Timeout: ${chunks}`);
+      socket.end();
+    }
+  }, 500);
   socket.write('echo SSH_NOW_SHORT_SESSION_ID=$SSH_NOW_SHORT_SESSION_ID\n');
 });
 
@@ -82,39 +99,54 @@ app.get("/", (_req, res) => {
   res.redirect(`/${generateLongSessionId()}`);
 });
 
-publicIp.v4().then(ip => {
-  const indexFile = fs.readFileSync("index.html", 'utf8');
-  app.get("/:long_session_id", (req, res) => {
-    if (process.env.LOCAL === "true") {
-      ip = "127.0.0.1";
-    }
-    const file = indexFile
-      .replace("{{PUBLIC_IP}}", ip)
-      .replace("{{SSH_NOW_SHORT_SESSION_ID}}", shortSessionIdFromLongSessionId(req.params.long_session_id))
-      .replace("{{WEBSOCKET_PROTOCOL}}", process.env.LOCAL === "true" ? "ws" : "wss");
-    res.send(file);
-  });
+let ip = "127.0.0.1";
+if (process.env.IP && process.env.IP.length > 0) {
+  ip = process.env.IP;
+}
+const indexFile = fs.readFileSync("index.html", 'utf8');
+app.get("/:long_session_id", (req, res) => {
+  if (!isValidLongSessionId(req.params.long_session_id)) {
+    res.redirect("/");
+    return;
+  }
+  const file = indexFile
+    .replace("{{PUBLIC_IP}}", ip)
+    .replace("{{SSH_NOW_SHORT_SESSION_ID}}", shortSessionIdFromLongSessionId(req.params.long_session_id))
+    .replace("{{WEBSOCKET_PROTOCOL}}", process.env.LOCAL === "true" ? "ws" : "wss");
+  res.send(file);
 });
 
 // Listen to a websocket given a session id.
 expressWs(app);
 app.ws("/ws/:long_session_id", (ws, req) => {
   const longSessionId = req.params.long_session_id;
+  if (!isValidLongSessionId(longSessionId)) {
+    ws.close();
+    return;
+  }
   const shortSessionId = shortSessionIdFromLongSessionId(longSessionId);
-  console.log(`New websocket for session ${longSessionId}`);
+  if (process.env.VERBOSE === "true") {
+    console.log(`New websocket for session ${longSessionId}`);
+  }
   webSocketsByShortSessionId[shortSessionId] = webSocketsByShortSessionId[shortSessionId] || [];
   webSocketsByShortSessionId[shortSessionId].push(ws);
   ws.on('message', function (msg) {
-    console.log(`Message received: ${msg}`);
+    if (process.env.VERBOSE === "true") {
+      console.log(`Message received: ${msg}`);
+    }
     if (remoteSocketsByShortSessionId[shortSessionId]) {
-      console.log(`Sending to remote socket: ${msg}`);
+      if (process.env.VERBOSE === "true") {
+        console.log(`Sending to remote socket: ${msg}`);
+      }
       for (const remoteSocket of remoteSocketsByShortSessionId[shortSessionId]) {
         remoteSocket.write(msg);
       }
     }
   });
   ws.on('close', function () {
-    console.log(`Websocket closed for session ${shortSessionId}`);
+    if (process.env.VERBOSE === "true") {
+      console.log(`Websocket closed for session ${shortSessionId}`);
+    }
     if (webSocketsByShortSessionId[shortSessionId]) {
       webSocketsByShortSessionId[shortSessionId].splice(webSocketsByShortSessionId[shortSessionId].indexOf(ws), 1);
       if (webSocketsByShortSessionId[shortSessionId].length === 0) {
@@ -127,7 +159,7 @@ app.ws("/ws/:long_session_id", (ws, req) => {
   }
 });
 
-const webPort = 3000;
+const webPort = 80;
 app.listen(webPort, () => {
   console.log(`App listening at http://localhost:${webPort}`);
 });
